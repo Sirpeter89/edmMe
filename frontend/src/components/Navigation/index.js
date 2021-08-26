4f36b20e4d795d82fa60b7eb36c5af069c7e3f2e
Justin <just2rules@gmail.com>
2021-08-25 20:06:16 -0700

    made nav buttons more appealing

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -36,12 +36,19 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-        <div className='loginButton'>
+        <div className="splash-buttons">
+          <NavLink className='all-events-button' to="/" style={{textDecoration: 'none'}}>All Events</NavLink>
+          <NavLink className='all-events-button' to="/signup" style={{textDecoration: 'none'}}>Add Events</NavLink>
+        </div>
+        <div className="splash-buttons">
+          <NavLink className='all-events-button' to="/" style={{textDecoration: 'none'}}>Sign Up/Login</NavLink>
+        </div>
+        {/* <div className='loginButton'>
           <NavLink to="/login" style={{textDecoration: 'none'}}>Log In</NavLink>
         </div>
         <div className='signupButton' >
           <NavLink to="/signup" style={{textDecoration: 'none'}}>Sign Up</NavLink>
-        </div>
+        </div> */}
       </>
     );
   }
